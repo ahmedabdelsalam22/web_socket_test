8a00cafd73e920d328f0c6da3e5db80c79ad0563
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2022-12-25 00:44:44 +0200

    add chat_screen.dart ui

--- lib/chat_app_socket_io/screen/chat_screen.dart
+++ lib/chat_app_socket_io/screen/chat_screen.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
   static const String ROUTE_ID = 'chat_screen';
 
   @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      /* appBar: AppBar(
+        title: ChatTitle(
+          chatUser: G.toChatUser,
+          userOnlineStatus: _userOnlineStatus,
+        ),
+      ),*/
+      body: SafeArea(
+        child: Container(
+          color: Colors.white,
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              /*    _chatList(),
+              _bottomChatArea(),*/
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
